4678168f9a91d19775b4dc4a528db0b71af55dd6
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-08-08 16:51:48 -0300

    Desenvolvendo o controller para realizar uma nova venda

---
+++ microservices-with-kafka-venda/src/main/java/br/com/vx/microservices/kafka/venda/controller/VendaController.java
@@ -0,0 +1,22 @@
+package br.com.vx.microservices.kafka.venda.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/venda")
+public class VendaController {
+
+    /* Sempre que eu for realizar essa venda eu quero postar no tópico que
+    * essa venda foi realizada e ele atualizar o estoque do produto */
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public ResponseEntity<Void> realizarVenda(@RequestBody String idProduto){
+        // realizando a postagem de uma nova mensagem no tópico
+        kafkaTemplate.send("estoque-topico", idProduto);
+        return ResponseEntity.ok().build();
+    }
+}
