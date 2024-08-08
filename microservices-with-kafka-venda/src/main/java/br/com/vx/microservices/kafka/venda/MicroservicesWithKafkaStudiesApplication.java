ceeedbeb8d3f0144dfbeff2925e4251b9020da41
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-08-08 12:51:51 -0300

    Estrutura inicial projeto microservice venda

---
+++ microservices-with-kafka-venda/src/main/java/br/com/vx/microservices/kafka/venda/MicroservicesWithKafkaStudiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.vx.microservices.kafka.venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesWithKafkaStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesWithKafkaStudiesApplication.class, args);
+	}
+
+}
