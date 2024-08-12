6495d3b433ba1298ecb7417b33f9e85f54bb98e0
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-08-12 10:58:49 -0300

    Estrutura inicial do microserviço de estoque

---
+++ microservices-with-kafka-estoque/src/test/java/br/com/vx/microservices/kafka/estoque/MicroservicesWithKafkaStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vx.microservices.kafka.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesWithKafkaStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- microservices-with-kafka-venda/src/main/java/br/com/vx/microservices/kafka/venda/controller/VendaController.java
+++ microservices-with-kafka-venda/src/main/java/br/com/vx/microservices/kafka/venda/controller/VendaController.java
@@ -2,6 +2,7 @@ package br.com.vx.microservices.kafka.venda.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class VendaController {
     * essa venda foi realizada e ele atualizar o estoque do produto */
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @PostMapping
     public ResponseEntity<Void> realizarVenda(@RequestBody String idProduto){
         // realizando a postagem de uma nova mensagem no tópico
         kafkaTemplate.send("estoque-topico", idProduto);
