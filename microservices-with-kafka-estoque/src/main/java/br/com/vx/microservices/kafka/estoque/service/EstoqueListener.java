8b71a0a900805f7670ac18ccea304d6d926d18b5
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-08-12 11:15:25 -0300

    Desenvolvendo as configurações e o service do meu microservice de estoque

---
+++ microservices-with-kafka-estoque/src/main/java/br/com/vx/microservices/kafka/estoque/service/EstoqueListener.java
@@ -0,0 +1,12 @@
+package br.com.vx.microservices.kafka.estoque.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstoqueListener {
+    @KafkaListener(topics = "product-update-topic", groupId = "estoque-group")
+    public void processarVenda(String mensagem) {
+        System.out.println("Venda recebida: " + mensagem);
+    }
+}
